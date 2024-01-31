5df6efef0fd1d9f98b247d4d3e02640d83cae1b1
Muhammed Kalkan <kalkanikbal@gmail.com>
2024-01-31 08:52:13 +0300

    add sd card boot

---
+++ Factory/config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export LD_LIBRARY_PATH=/mnt/Factory/custom/lib
+
+/usr/sbin/net_manage.sh &
+ak_adec_demo 16000 2 aac /usr/share/audio_file/common/didi.aac
+/mnt/Factory/custom/ptz_daemon & 
+/mnt/Factory/custom/busybox httpd -p 8080 -h /mnt/Factory/custom/web_interface/www &
+sleep 30
+/mnt/Factory/custom/rtsp &
