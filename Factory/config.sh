5eb873d984eacbfb9c5a100f2b3ea3140c53e579
Muhammed Kalkan <kalkanikbal@gmail.com>
2024-01-31 09:08:49 +0300

    Update config.sh

--- Factory/config.sh
+++ Factory/config.sh
@@ -3,6 +3,7 @@
 export LD_LIBRARY_PATH=/mnt/Factory/custom/lib
 
 /usr/sbin/net_manage.sh &
+telnetd &
 ak_adec_demo 16000 2 aac /usr/share/audio_file/common/didi.aac
 /mnt/Factory/custom/ptz_daemon & 
 /mnt/Factory/custom/busybox httpd -p 8080 -h /mnt/Factory/custom/web_interface/www &
