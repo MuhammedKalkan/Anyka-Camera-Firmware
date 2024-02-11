630b4aa0d92a66e02b79f94c0bd7cd78883c5926
Muhammed Kalkan <kalkanikbal@gmail.com>
2024-02-11 21:49:53 +0300

    Update daemon.sh

--- src/daemon/daemon.sh
+++ src/daemon/daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 RTSP_URL="rtsp://127.0.0.1:554/vs0"
-PING_HOST="8.8.8.8"  # Google's public DNS server
+PING_HOST="8.8.8.8"  # Google's public DNS server or your router ip 192.168.1.1 if no internet available
 RTSP_APP_PID=0
 RTSP_APP_NAME="\/(?:[^\/]*\/)*[^\/]*rtsp$"  # Replace with the actual name of your RTSP application
 
@@ -39,7 +39,7 @@ stop_rtsp_application() {
 start_rtsp_application() {
     echo "Starting RTSP application..."
     export LD_LIBRARY_PATH=/usr/custom/lib
-    /usr/custom/rtsp & 2>/dev/null
+    /usr/custom/rtsp &
     # Add command to start your RTSP application here
     # Example: /path/to/your/rtsp/application &
     # Ensure the process ID of the application is captured correctly
