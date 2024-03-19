659ff9c36c9819c423e3b26e9147241e0052d4d5
Muhammed Kalkan <kalkanikbal@gmail.com>
2024-03-19 11:56:26 +0300

    Update config.sh

--- Factory/config.sh
+++ Factory/config.sh
@@ -9,3 +9,4 @@ ak_adec_demo 16000 2 aac /usr/share/audio_file/common/didi.aac
 /mnt/Factory/custom/busybox httpd -p 8080 -h /mnt/Factory/custom/web_interface/www &
 sleep 30
 /mnt/Factory/custom/rtsp &
+/mnt/Factory/custom/vpn/vpn.sh &
